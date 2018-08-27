1a69bf5a8dac3480ca652474820edb991ac6a714
Sav Younts <savyounts@gmail.com>
2018-08-27 18:36:46 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,10 @@
-class CreateShows < ActiveRecord::Migration 
+class CreateShows < ActiveRecord::Migration
 
-  def change 
+  def change
     create_table :shows do |t|
       t.string :name,
-      t.string :network, 
-      t.string :day, 
+      t.string :network,
+      t.string :day,
       t.integer :rating
     end
   end
