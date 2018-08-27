e1ccfedb82688db390601f0663801a64886088bd
Sav Younts <savyounts@gmail.com>
2018-08-27 18:52:37 +0000

    automatically backed up by learn

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -5,7 +5,8 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where(Show.highest_rating)
+    rating = Show.highest_rating
+    Show.where('rating = ?', rating)
   end
 
   def self.lowest_rating
