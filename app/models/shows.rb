ae46b70e2001868142108fd48aec5aa1100b0bb8
Sav Younts <savyounts@gmail.com>
2018-08-27 18:56:23 +0000

    automatically backed up by learn

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -17,9 +17,11 @@ class Show < ActiveRecord::Base
   end
 
   def self.ratings_sum
+    Show.sum(:rating)
   end
 
   def self.popular_shows
+    Show.where("rating > '5'")
   end
 
   def self.shows_by_alphabetical_order
